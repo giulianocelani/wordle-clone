0179209bbe670b1e7cab0fcb54d7945f59a2c21f
Giuliano Celani <celanigiuliano@gmail.com>
2022-03-31 00:01:10 +1100

    style: change miss to gray

--- src/components/Letter/index.tsx
+++ src/components/Letter/index.tsx
@@ -8,7 +8,7 @@ type IProps = {
 const StateToColorMapping: Record<LetterState, string> = {
 	[LetterState.PRESENT]: 'bg-yellow-500 border-yellow-500',
 	[LetterState.MATCH]: 'bg-green-500 border-green-500',
-	[LetterState.MISS]: 'bg-red-500 border-red-500'
+	[LetterState.MISS]: 'bg-gray-500 border-gray-500'
 };
 
 const Letter = ({ letter, state }: IProps) => {
