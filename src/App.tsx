e81efc688455e26e8c71a1471d9d88f91db4f1bb
Giuliano Celani <celanigiuliano@gmail.com>
2022-03-30 16:58:16 +1100

    style: added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 const App = () => {
-	return <div />;
+	return (
+		<div className='container mx-auto overflow-hidden fixed w-full h-full'>
+			<h1 className='text-5xl font-bold text-center my-4 pb-4 border-b'>
+				Wordle Clone
+			</h1>
+			<div className='my-5'>
+				<p>This is a clone of the popular wordle game.</p>
+			</div>
+		</div>
+	);
 };
 
 export default App;
