85bc7a70bf20751d8d8aded696263e16900b1de5
Giuliano Celani <celanigiuliano@gmail.com>
2022-03-30 18:02:05 +1100

    feat: add 5 letter word bank and utils

--- src/components/Guess/index.tsx
+++ src/components/Guess/index.tsx
@@ -1,6 +1,5 @@
+import { MAX_WORD_LENGTH } from '../../utils';
 import Letter from '../Letter';
-
-const MAX_GUESS_LENGTH = 5;
 
 type IProps = {
 	guess: string;
@@ -11,7 +10,7 @@ const Guess = ({ guess }: IProps) => {
 
 	return (
 		<div className='grid grid-cols-5 gap-4'>
-			{[...letters, ...Array(MAX_GUESS_LENGTH - letters.length).fill('')].map(
+			{[...letters, ...Array(MAX_WORD_LENGTH - letters.length).fill('')].map(
 				(letter, index) => (
 					// eslint-disable-next-line react/no-array-index-key
 					<Letter key={`guess-${index}`} letter={letter} />
