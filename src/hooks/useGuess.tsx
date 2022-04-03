3269c6731822df2b1192b8737d77cc85a0af7042
Giuliano Celani <celanigiuliano@gmail.com>
2022-04-03 16:06:26 +1000

    feat: only allow needed key characters

--- src/hooks/useGuess.tsx
+++ src/hooks/useGuess.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { MAX_WORD_LENGTH } from '../utils';
 
@@ -8,32 +8,54 @@ const useGuess = (): [
 	(letter: string) => void
 ] => {
 	const [guess, setGuess] = useState<string>('');
+	const previousKey = useRef<string>('');
+
+	const isValidKey = (key: string): boolean => {
+		if (key === 'Backspace' || key === 'Enter') {
+			return true;
+		}
+
+		if (key.includes('Key')) {
+			if (
+				previousKey.current.includes('Key') ||
+				previousKey.current === '' ||
+				previousKey.current === 'Backspace' ||
+				previousKey.current === 'Enter'
+			) {
+				return true;
+			}
+		}
+
+		return false;
+	};
 
 	const addGuessLetter = (letter: string): void => {
 		setGuess((prevGuess) => {
+			const newGuess = (
+				letter.length === 1 && prevGuess.length !== MAX_WORD_LENGTH
+					? prevGuess + letter
+					: prevGuess
+			).toUpperCase();
+
 			if (letter === 'Backspace') {
 				return prevGuess.slice(0, -1);
 			}
 			if (letter === 'Enter' && prevGuess.length === MAX_WORD_LENGTH) {
 				return '';
 			}
-			const newGuess = prevGuess + letter.toUpperCase();
-			if (newGuess.length <= MAX_WORD_LENGTH) {
+
+			if (newGuess.length === MAX_WORD_LENGTH) {
 				return newGuess;
 			}
-			return prevGuess;
+			return newGuess;
 		});
 	};
 
 	const onKeyDown = (event: KeyboardEvent) => {
-		if (
-			event.key === 'Backspace' ||
-			event.key === 'Enter' ||
-			(event.keyCode >= 65 && event.keyCode <= 90) ||
-			(event.keyCode >= 97 && event.keyCode <= 122)
-		) {
+		if (isValidKey(event.code)) {
 			addGuessLetter(event.key);
 		}
+		previousKey.current = event.code;
 	};
 
 	useEffect(() => {
@@ -42,8 +64,6 @@ const useGuess = (): [
 			window.removeEventListener('keydown', onKeyDown);
 		};
 	}, []);
-
-	console.log(guess);
 
 	return [guess, setGuess, addGuessLetter];
 };
